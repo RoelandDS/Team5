8e85a455728c03e8ebe0663aa337dc16b7ad8d26
Rutger Benoot <rutgerbenoot@hotmail.com>
2015-12-03 15:40:46 +0100

    adres aanpassen

--- htdocs/app.js
+++ htdocs/app.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:4000");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
   next();
