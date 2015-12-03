07546ff981218abbad1fb1663ad5b3e1127c7c85
Gaetano Herman <hermangaetano@gmail.com>
2015-12-03 12:43:59 +0100

    no message

--- stap1/controllers.js
+++ stap1/controllers.js
@@ -1,19 +1,32 @@
-var app = angular.module('hackthefuture')
-  .constant('APIKEY', '13c6eb73373cbacf3052be6b1221247')
-  .constant('LIGHTNUMBER', '12');
+var app = angular.module('hackthefuture');
 
 app.controller('MainCtrl', [
-'$scope', 'colour',
-function($scope, colour){
-  $scope.colour = colour.colour;
+  '$scope', 'colour',
+  function($scope, colour) {
+    $scope.colour = colour.colour;
 
-  $scope.changeColour = function() {
-    if(!$scope.rood || !$scope.groen || !$scope.blauw ) { return; }
+    $scope.changeColour = function(colour) {
+      switch (colour) {
+        case rood:
+          colour.changeColour();
+          break;
+        case groen:
 
-    colour.changeColour($scope.rood, $scope.groen, $scope.blauw);
+          break;
+        case geel:
+
+          break;
+        case blauw:
+
+          break;
+        default:
+
+      }
+      colour.changeColour($scope.rood, $scope.groen, $scope.blauw);
+    }
+
+    $scope.turnOff = function() {
+
+    }
   }
-
-  $scope.turnOff = function() {
-
-  }
-}]);
+]);
